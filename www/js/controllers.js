71b528865fc32d291f528f774d0362b865146844
Ratel <Callaghanlj@gmail.com>
2016-07-18 14:31:46 -0700

    PGT-49  This is the final version. Does everything its supposed to and looks really nice.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -462,53 +462,66 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
  })
 
-
- .controller('NotificationCtrl', function($scope, $cordovaLocalNotification, $ionicPlatform)
+.controller('settingsCtrl', function($scope, $cordovaLocalNotification, $ionicPlatform, $window)
    {
-       $ionicPlatform.ready(function ()
-       {
-           $scope.scheduleSingleNotification = function ()
-           {
+   $ionicPlatform.ready(function ()
+          {
+              $scope.scheduleSingleNotification = function ()
+              {
 
-             $cordovaLocalNotification.schedule(
-             {
-               id: 1,
-               title: 'PG Transit',
-               text: 'You have enabled Notifications',
-               data: {
-                 customProperty: 'custom value'
-               }
-             }).then(function (result) {
-               console.log('Notification 1 triggered');
-             });
-           };
-            $scope.scheduleLocationNotification = function ()
-                      {
+                $cordovaLocalNotification.schedule(
+                {
+                  id: 1,
+                  title: 'PG Transit',
+                  text: 'You have enabled Notifications',
+                  data: {
+                    customProperty: 'custom value'
+                  }
+                }).then(function (result) {
+                  console.log('Notification 1 triggered');
+                });
+              };
+               $scope.scheduleLocationNotification = function ()
+              {
 
-                        $cordovaLocalNotification.schedule(
-                        {
-                          id: 2,
-                          title: 'PG Transit',
-                          text: 'You have enabled Locations',
-                          data: {
-                            customProperty: 'custom value'
-                          }
-                        }).then(function (result) {
-                          console.log('Notification 2 triggered');
-                        });
-                      };
-       });
-   })
-
-   .controller('settingsTestctrl', function($scope, $cordovaLocalNotification, $ionicPlatform)
-   {
-
-     $scope.pushNotificationChange = function() {
+              $cordovaLocalNotification.schedule(
+              {
+                id: 2,
+                title: 'PG Transit',
+                text: 'You have enabled Locations',
+                data:
+                {
+                    customProperty: 'custom value'
+                }
+                }).then(function (result)
+                {
+                   console.log('Notification 2 triggered');
+                });
+                };
+          });
+     $scope.pushNotificationChange = function()
+     {
        console.log('Push Notification Change', $scope.pushNotification.checked);
+       if ($scope.pushNotification.checked == true)
+       {
+           $scope.scheduleSingleNotification();
+           console.log('Notifications enabled taking action');
+       }
      };
-
-     $scope.pushNotification = { checked: true };
-
+     $scope.enableLocationChange = function()
+     {
+        console.log('Enable Location Change', $scope.pushLocationNotification.checked);
+        if($scope.pushLocationNotification.checked == true)
+        {
+            $scope.scheduleLocationNotification();
+            console.log('Location enabled taking action')
+        }
+     }
+      $scope.userName = localStorage.getItem('username');
+        $scope.saveName = function()
+        {
+          localStorage.setItem('username', document.getElementById('userNameBox').value);
+        };
    })
 
 
