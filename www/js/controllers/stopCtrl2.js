fae150e440a50bb45bc15c11d9f6fa7bbcc15072
Mike Drakos <Mike@Mikes-MacBook-Pro.local>
2016-11-14 20:30:41 -0800

    Reformating and fixing merge conflicts

--- www/js/controllers/stopCtrl2.js
+++ www/js/controllers/stopCtrl2.js
@@ -1,9 +1,9 @@
 angular.module('starter.controllers').controller("stopCtrl2", function ($scope, stopService1)
 {
-    var promise = stopService1.getCo();
-    promise.then(function (data)
-    {
-        $scope.co = data.data;
-        console.log($scope.co);
-    });
+  var promise = stopService1.getCo();
+  promise.then(function (data)
+  {
+    $scope.co = data.data;
+    console.log($scope.co);
+  });
 });
